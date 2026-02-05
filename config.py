7da65f0a543bdcdfaa70e55aa9ca1de3398ea9b3
garpil12 <storegarpil@gmail.com>
2026-02-05 14:19:48 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+API_ID = 123456
+API_HASH = "API_HASH_KAMU"
+BOT_TOKEN = None        # isi kalau bot token
+SESSION = "manajemen"  # session userbot
+
+OWNER_ID = [123456789]
+CMD_PREFIX = ["!", ".", "/"]
+
+LOG_CHAT = None  # isi chat id buat log (opsional)
